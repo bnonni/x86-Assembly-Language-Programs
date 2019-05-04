6a2896dc2053375ab4e7913ceebe7f0f04c17511
bryan <bryanwnonni@gmail.com>
2019-05-03 20:15:32 -0400

    Assgn 7 P1

--- Sandbox/AddTwo/Nonni1.asm
+++ Sandbox/AddTwo/Nonni1.asm
@@ -1,5 +1,7 @@
 ; Student: Bryan Nonni
-; Chapter 3 example
+; Class: CSC3210 - TR 1545
+; Assignment#: 7 - Program 1												
+; Description: This program implements the mathmatical expression val1 = (val3 – val2) * (val4 / val3) - (val2 * 2)
 
 .386
 .model flat,stdcall
@@ -9,29 +11,42 @@ ExitProcess proto,dwExitCode:dword
 Include Irvine32.inc
 
 .data
-val1 SDWORD ?
-val2 SDWORD ?
-val3 SDWORD ?
-val4 SDWORD ?
-myPrompt BYTE "Enter Signed Integer: ", 0
-
-
-;  val1 = (val3 – val2) * (val4 / val3) - (val2 * 2)
+val1 SDWORD 0
+val2 SDWORD 0
+val3 SDWORD 0
+val4 SDWORD 0
+prompt BYTE "Enter Signed Integer: ", 0
 
 .code
 main proc
-	
+	mov eax, 0					; Clear eax
+	mov ebx, 0					; Clear ebx
+	mov ecx, 0					; Clear ecx
+	mov edx, 0					; Clear edx
 	mov esi, OFFSET val2		; Move address of val2 to esi
-	mov ecx, 3
+	mov ecx, 3					; Move 3 to ecx for loop counter
  
  readIntegers:
 	call Clrscr					; Clear screen
-	mov edx, offset myPrompt	; Move prompt to edx to be displayed
+	mov edx, offset prompt	; Move prompt to edx to be displayed
 	call Writestring			; Display prompt
 	call Readint				; Read int to eax
-	mov [esi], eax				; mov eax value to [esi] = val2
-	inc esi						; Increment esi to val3, val4
-	loop readIntegers			; Loop back to beginning
+	mov [esi], eax				; mov eax value to [esi]
+	add esi, 4					; Increment esi to next val
+    loop readIntegers			; Loop back to beginning
+
+	mov edx, 0					; Clear address of prompt
+	mov eax, val4				; Move val4 to eax
+	idiv val3					; Signed divide eax by val3
+	mov ebx, eax				; Move eax to ebx for later use
+	mov eax, val3				; Move val3 to eax
+	sub eax, val2				; Subtract val2 from eax
+	imul ebx					; Signed multiply eax by ebx
+	mov ecx, eax				; Move eax to ecx
+	mov eax, 2					; Move 2 to eax
+	imul val2					; Signed multiply eax by val2
+	sub ecx, eax				; Subtract eax from ecx
+	mov val1, ecx				; Move ecx to val1
 
  exit
 main endp
